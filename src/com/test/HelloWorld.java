2a86d444eb1457744eebab7fb3ea7cf8b0e93887
zhangfu <1654377514@qq.com>
2019-09-10 19:40:55 +0800

    git提交测试

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.test;
+
+/**
+ * 项目名称:GitTest
+ * 类 名 称:HelloWorld
+ * 类 描 述:TODO
+ * 创建时间:2019/9/10 19:09
+ * 创 建 人:zhangfu
+ */
+public class HelloWorld {
+    public static void main (String [] args){
+        System.out.println("hello world！");
+    }
+}
